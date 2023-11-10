5cb35a6a4de0a801a1e7a48a3291b8b41a6882fe
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-11-10 21:57:01 +0900

    feat(config): add BadgeConfig

--- src/main/java/christmas/config/BadgeConfig.java
+++ src/main/java/christmas/config/BadgeConfig.java
@@ -1,4 +1,18 @@
 package christmas.config;
 
 public enum BadgeConfig {
+    STAR(5000),
+    TREE(10000),
+    SANTA(20000),
+    ;
+
+    private final int conditionValue;
+
+    BadgeConfig(int conditionValue) {
+        this.conditionValue = conditionValue;
+    }
+
+    public int getConditionValue() {
+        return conditionValue;
+    }
 }
