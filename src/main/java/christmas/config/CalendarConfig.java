e4bb5469d678e5bb8b77d1e15f25580b4beea7e4
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-11-10 22:36:50 +0900

    feat(Config): add Calendar/Day/DecemberSpecialDays Config

--- src/main/java/christmas/config/CalendarConfig.java
+++ src/main/java/christmas/config/CalendarConfig.java
@@ -1,4 +1,15 @@
 package christmas.config;
 
 public enum CalendarConfig {
+    DECEMBER(31);
+
+    private final int final_date;
+
+    CalendarConfig(int final_date) {
+        this.final_date = final_date;
+    }
+
+    public int getFinal_date() {
+        return final_date;
+    }
 }
---
+++ src/main/java/christmas/config/DayConfig.java
@@ -0,0 +1,11 @@
+package christmas.config;
+
+public enum DayConfig {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
