d6e8bb3eb50dce7a545f9462026e671e96388848
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-11-13 22:33:12 +0900

    feat(DateOfVisit): 방문날짜 기능 추가

--- src/main/java/christmas/config/ErrorMessageConfig.java
+++ src/main/java/christmas/config/ErrorMessageConfig.java
@@ -1,20 +1,21 @@
 package christmas.config;
 
-public enum ErrorConfig {
+public enum ErrorMessageConfig {
     COMMON_ERROR_MESSAGE("[ERROR] "),
     NOT_VALIDATE_ORDER_ERROR_MESSAGE("유효하지 않은 주문입니다."),
     NOT_NULL_ORDER_ERROR_MESSAGE("아무것도 주문하지 않았습니다. "),
     NOT_ZERO_ORDER_ERROR_MESSAGE("주문은 1개 이상이어야 합니다. "),
-    RANGE_TOTAL_ORDER_ERROR_MESSAGE("총 주문은 1개 이상 20개 이하여야 합니다. ");;
+    RANGE_TOTAL_ORDER_ERROR_MESSAGE("총 주문은 1개 이상 20개 이하여야 합니다. "),
+    NOT_VALID_DATE_OF_VISIT_ERROR_MESSAGE("유효하지 않은 날짜입니다.");
 
     private final String message;
 
-    ErrorConfig(String message) {
+    ErrorMessageConfig(String message) {
         this.message = message;
     }
 
     public String getMessage() {
-        return ErrorConfig.COMMON_ERROR_MESSAGE.message + message;
+        return ErrorMessageConfig.COMMON_ERROR_MESSAGE.message + message;
     }
 
     public String getSubMessage() {
--- src/main/java/christmas/model/DateOfVisit.java
+++ src/main/java/christmas/model/DateOfVisit.java
@@ -1,2 +1,26 @@
-package christmas.model;public class DateOfVisit {
+package christmas.model;
+
+import christmas.enums.MonthOfYear;
+import christmas.exception.DateOfVisitException;
+
+public class DateOfVisit {
+    private final int FIRST_DAY = 1;
+    private final MonthOfYear month;
+    private final int day;
+
+    public DateOfVisit(MonthOfYear month, int day) {
+        validate(month, day);
+        this.month = month;
+        this.day = day;
+    }
+
+    private void validate(MonthOfYear month, int day) {
+        validateRangeOfMonth(month, day);
+    }
+
+    private void validateRangeOfMonth(MonthOfYear month, int day) {
+        if (day < FIRST_DAY && day > month.getFinalDate()) {
+            throw new DateOfVisitException();
+        }
+    }
 }
