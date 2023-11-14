64696cda6bf83b2c538e9fbf2ae3a91661b26c5d
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-11-14 20:39:10 +0900

    feat(input): input view 기능 추가

--- src/main/java/christmas/enums/MonthOfYear.java
+++ src/main/java/christmas/enums/MonthOfYear.java
@@ -1,15 +1,21 @@
 package christmas.enums;
 
 public enum MonthOfYear {
-    DECEMBER(31);
+    DECEMBER(31, 12);
 
     private final int finalDate;
+    private final int month;
 
-    MonthOfYear(int finalDate) {
+    MonthOfYear(int finalDate, int month) {
         this.finalDate = finalDate;
+        this.month = month;
     }
 
     public int getFinalDate() {
         return finalDate;
+    }
+
+    public int getMonth() {
+        return month;
     }
 }
--- src/main/java/christmas/view/input/InputView.java
+++ src/main/java/christmas/view/input/InputView.java
@@ -1,4 +1,24 @@
 package christmas.view.input;
 
+import camp.nextstep.edu.missionutils.Console;
+import christmas.enums.MonthOfYear;
+
 public class InputView {
+    private final int MONTH = MonthOfYear.DECEMBER.getMonth();
+    private final String DATE_OF_VISIT_REQUEST_MESSAGE = "%d월 중 식당 예상 방문 날짜는 언제인가요?";
+    private final String ORDER_MENU_AND_NUMBER_REQUEST_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+
+    public String requestDateOfVisit() {
+        System.out.println(String.format(DATE_OF_VISIT_REQUEST_MESSAGE, MONTH));
+        return userInput();
+    }
+
+    public String requestOrderMenuAndNumber() {
+        System.out.println(ORDER_MENU_AND_NUMBER_REQUEST_MESSAGE);
+        return userInput();
+    }
+
+    private static String userInput() {
+        return Console.readLine().trim();
+    }
 }
